ceff0644ff638b38915f448824b02c0d78f348b4
AHAPX <anarchy.b@gmail.com>
2016-07-21 06:42:29 -0400

    added base tests

---
+++ tests.py
@@ -0,0 +1,51 @@
+from unittest import TestCase
+
+import trafaret
+
+from smtpush import validate
+
+
+class TestSMTPush(TestCase):
+
+    def test_validate_errors(self):
+        with self.assertRaises(trafaret.DataError):
+            validate({})
+        with self.assertRaises(trafaret.DataError):
+            validate({'to': ['t@m.com'], 'body': 'b'})
+        with self.assertRaises(trafaret.DataError):
+            validate({'to': ['t@m.com'], 'subj': 's'})
+        with self.assertRaises(trafaret.DataError):
+            validate({'to': ['t@g'], 'body': 'b', 'subj': 's'})
+        with self.assertRaises(trafaret.DataError):
+            validate({'to': ['t@g.com'], 'body': 'b', 'subj': 's', 'from': 'f'})
+        with self.assertRaises(trafaret.DataError):
+            validate({'to': ['t@g.com'], 'body': 'b', 'subj': 's', 'cc': 'c@g.com'})
+        with self.assertRaises(trafaret.DataError):
+            validate({'to': ['t@g.com'], 'body': 'b', 'subj': 's', 'bcc': 'c@g.com'})
+        with self.assertRaises(trafaret.DataError):
+            validate({'to': ['t@g.com'], 'body': 'b', 'subj': 's', 'html': 6})
+
+    def test_validate_success(self):
+        # short
+        data = {'to': ['t@g.com'], 'body': 'b', 'subj': 's'}
+        self.assertEqual(validate(data), data)
+        # full
+        data = {
+            'to': ['t@g.com'],
+            'body': 'b', 
+            'subj': 's',
+            'from': 'f@g.com',
+            'cc': ['c@g.com'],
+            'bcc': ['b@g.com'],
+            'html': True,
+        }
+        res = {
+            'to': ['t@g.com'],
+            'body': 'b', 
+            'subj': 's',
+            'sender': 'f@g.com',
+            'cc': ['c@g.com'],
+            'bcc': ['b@g.com'],
+            'html': True,
+        }
+        self.assertEqual(validate(data), res)
